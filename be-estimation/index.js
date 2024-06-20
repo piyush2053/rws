09249237cc23b7eeafd6966b9d0798b3a884fc03
piyush.patel <piyush.patel@rws.com>
2024-06-20 21:51:27 +0530

    Fixes 1.1.9

--- be-estimation/index.js
+++ be-estimation/index.js
@@ -11,59 +11,52 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/estimate", async (req, res) => {
-    const {
-        effortDays,
-        numberOfResources,
-        numberOfTesters,
-        numberOfProjectManagers,
-        projectName,
-        assumptions,
-        queries
-    } = req.body;
-
-    // Load the template Excel file
-    const templateFilePath = path.join(__dirname, 'template.xlsx');
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.readFile(templateFilePath);
-
-    // Modify sheet 1 (WBS)
-    const sheet1 = workbook.getWorksheet('WBS');
-    sheet1.getCell('E8').value = effortDays;
-    sheet1.getCell('D2').value = projectName;
-
-    // Modify sheet 2 (Schedule)
-    const sheet2 = workbook.getWorksheet('Schedule');
-    sheet2.getCell('B2').value = projectName;
-    const resourcesStartRow = 7;
-    for (let i = 0; i < numberOfResources; i++) {
-        sheet2.getCell(`B${resourcesStartRow + i}`).value = `Resource ${i + 1}`;
+    try {
+        const {
+            effortDays,
+            numberOfResource,
+            numberOfTester,
+            numberOfProjectManager,
+            projectName,
+            assumptions,
+            queries
+        } = req.body;
+        const templateFilePath = path.join(__dirname, 'template.xlsx');
+        const workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.readFile(templateFilePath);
+    
+        const sheet1 = workbook.getWorksheet('WBS');
+        sheet1.getCell('E8').value = effortDays;
+        sheet1.getCell('D2').value = projectName;
+    
+        const sheet2 = workbook.getWorksheet('Schedule');
+        sheet2.getCell('B2').value = projectName;
+        sheet2.getCell("B7").value = `Resources - (${numberOfResource})`;
+        sheet2.getCell("B8").value = `Tester - (${numberOfTester})`;
+        sheet2.getCell("B9").value = `Project Manager - (${numberOfProjectManager})`;
+    
+        const sheet3 = workbook.getWorksheet('Assumption & Queries');
+        sheet3.getCell("D2").value = projectName
+    
+        for (let i = 0; i < assumptions.length; i++) {
+            sheet3.getCell(`C${4 + i}`).value = assumptions[i];
+        }
+        for (let i = 0; i < queries.length; i++) {
+            sheet3.getCell(`F${4 + i}`).value = queries[i];
+        }
+    
+        const outputFile = path.join(__dirname, `output_${Date.now()}.xlsx`);
+    
+        await workbook.xlsx.writeFile(outputFile);
+    
+        const fileContents = fs.readFileSync(outputFile);
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', 'attachment; filename=estimate.xlsx');
+        res.send(fileContents);
+        fs.unlinkSync(outputFile);
+    } catch (error) {
+        res.status(500).send("Something went wrong");
     }
-
-    // Modify sheet 3 (Assumption & Queries)
-    const sheet3 = workbook.getWorksheet('Assumption & Queries');
-    // Write assumptions starting from C4
-    sheet3.getCell("D2").value = projectName
-
-    for (let i = 0; i < assumptions.length; i++) {
-        sheet3.getCell(`C${4 + i}`).value = assumptions[i];
-    }
-    // Write queries starting from C8
-    for (let i = 0; i < queries.length; i++) {
-        sheet3.getCell(`F${4 + i}`).value = queries[i];
-    }
-
-    // Generate a temporary file path for the new Excel file
-    const outputFile = path.join(__dirname, `output_${Date.now()}.xlsx`);
-
-    // Save the workbook to the temporary file
-    await workbook.xlsx.writeFile(outputFile);
-
-    // Stream the generated Excel file as a response
-    const fileContents = fs.readFileSync(outputFile);
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader('Content-Disposition', 'attachment; filename=estimate.xlsx');
-    res.send(fileContents);
-    fs.unlinkSync(outputFile);
 });
 
 const PORT = process.env.PORT || 8080;
--- estimation-helper/src/App.tsx
+++ estimation-helper/src/App.tsx
@@ -3,9 +3,12 @@ import rwsLogo from './assets/rwsbanner2.png'
 import add from './assets/add.png'
 import del from './assets/del.png'
 import excel from './assets/excel.png'
+import pp from './assets/ppIntials.png'
+
 import { URL, initialFormData } from './utils/constants/contants';
 const Form = () => {
   const [loading, setLoading] = useState<any>(false);
+  const [flip, setFlip] = useState<any>(false);
   const [error, setError] = useState<any>(false);
   const [formData, setFormData] = useState<any>(initialFormData);
 
@@ -25,7 +28,7 @@ const Form = () => {
   };
 
   const deleteArrayField = (index: number, field: string) => {
-    if (formData[field].length > 1) { 
+    if (formData[field].length > 1) {
       const newArray = [...formData[field]];
       newArray.splice(index, 1);
       setFormData({ ...formData, [field]: newArray });
@@ -34,6 +37,7 @@ const Form = () => {
 
   const handleSubmit = async (e: any) => {
     setLoading(true)
+    setFlip(true)
     setError(false)
     e.preventDefault();
     try {
@@ -49,11 +53,12 @@ const Form = () => {
         body: JSON.stringify(formData),
       });
 
-      if (!response.ok) {
+      if (!response.ok || response.status !== 200) {
         setLoading(false)
+        setFlip(false)
         setError(true)
         throw new Error('Failed to fetch');
-      }else{
+      } else {
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -65,6 +70,7 @@ const Form = () => {
         window.URL.revokeObjectURL(url);
         document.body.removeChild(a);
         setLoading(false)
+        setFlip(false)
         setError(false)
       }
     } catch (error) {
@@ -75,83 +81,83 @@ const Form = () => {
 
   return (
     <div className="animate-fade max-w-md mx-auto my-10 bg-white p-8 rounded-md shadow-md justify-center">
-      <img alt='logo' src={rwsLogo} className='mb-5 rounded-lg'></img>
+      <img alt='logo' src={flip ? pp : rwsLogo} className='animate-fade mb-5 rounded-lg relative cursor-pointer' onClick={() => setFlip(!flip)} ></img>
       <form onSubmit={handleSubmit} >
-      <div className='flex gap-5'>
-        <div>
-        {[
-          { label: 'Project Name', name: 'projectName' },
-          { label: 'Effort Days', name: 'effortDays', type: 'number' },
-          { label: 'Number of Resource', name: 'numberOfResource', type: 'number' },
-          { label: 'Number of Tester', name: 'numberOfTester', type: 'number' },
-          { label: 'Number of Project Manager', name: 'numberOfProjectManager', type: 'number' }
-        ].map((field, index) => (
-          <div className="mb-4" key={index}>
-            <label htmlFor={field.name} className="block text-sm font-medium text-gray-700">
-              {field.label}
-            </label>
-            <input
-              type={field.type || 'text'}
-              id={field.name}
-              name={field.name}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              onChange={handleChange}
-              required
-            />
-          </div>
-        ))}
-        </div>
-        <div>
-        {['assumptions', 'queries'].map((field, index) => (
-          <div className="mb-4" key={index}>
-            <label className="block text-sm font-medium text-gray-700">{field.charAt(0).toUpperCase() + field.slice(1)}</label>
-            {formData[field].map((value: string, idx: number) => (
-              <div className="flex mb-2" key={idx}>
+        <div className='flex gap-5'>
+          <div>
+            {[
+              { label: 'Project Name', name: 'projectName' },
+              { label: 'Effort Days', name: 'effortDays', type: 'number' },
+              { label: 'Number of Resource', name: 'numberOfResource', type: 'number' },
+              { label: 'Number of Tester', name: 'numberOfTester', type: 'number' },
+              { label: 'Number of Project Manager', name: 'numberOfProjectManager', type: 'number' }
+            ].map((field, index) => (
+              <div className="mb-4" key={index}>
+                <label htmlFor={field.name} className="block text-sm font-medium text-gray-700">
+                  {field.label}
+                </label>
                 <input
-                  type="text"
-                  value={value}
+                  type={field.type || 'text'}
+                  id={field.name}
+                  name={field.name}
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  onChange={(e) => handleArrayChange(e, idx, field)}
+                  onChange={handleChange}
                   required
                 />
-                {formData[field].length > 1 &&
-                <button
-                  type="button"
-                  style={{zIndex:"999999", marginLeft:'-30px'}}
-                  className='my-auto'
-                  onClick={() => deleteArrayField(idx, field)}
-                >
-                  <img alt='del' src={del} className='h-5 w-5 rounded-full'/>
-                </button>}
               </div>
             ))}
-            <button
-              type="button"
-              className="w-fullhover:bg-gray-300"
-              onClick={() => addArrayField(field)}
-            >
-              <div className='flex'>
-              <img alt='add' src={add} className='h-5 w-5 rounded-full'/>
-              <p className='text-gray-700 text-xs pl-2 py-auto'>Add More {field.charAt(0).toUpperCase() + field.slice(1)}</p>
-              </div>
-            </button>
           </div>
-        ))}
-        </div>
+          <div>
+            {['assumptions', 'queries'].map((field, index) => (
+              <div className="mb-4" key={index}>
+                <label className="block text-sm font-medium text-gray-700">{field.charAt(0).toUpperCase() + field.slice(1)}</label>
+                {formData[field].map((value: string, idx: number) => (
+                  <div className="flex mb-2" key={idx}>
+                    <input
+                      type="text"
+                      value={value}
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      onChange={(e) => handleArrayChange(e, idx, field)}
+                      required
+                    />
+                    {formData[field].length > 1 &&
+                      <button
+                        type="button"
+                        style={{ zIndex: "999999", marginLeft: '-30px' }}
+                        className='my-auto'
+                        onClick={() => deleteArrayField(idx, field)}
+                      >
+                        <img alt='del' src={del} className='h-5 w-5 rounded-full' />
+                      </button>}
+                  </div>
+                ))}
+                <button
+                  type="button"
+                  className="w-fullhover:bg-gray-300"
+                  onClick={() => addArrayField(field)}
+                >
+                  <div className='flex'>
+                    <img alt='add' src={add} className='h-5 w-5 rounded-full' />
+                    <p className='text-gray-700 text-xs pl-2 py-auto'>Add More {field.charAt(0).toUpperCase() + field.slice(1)}</p>
+                  </div>
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="mb-4">
           <button
             type="submit"
-            className={`${loading ? "w-full bg-[white] border-2 border-[#007373] text-white py-2 px-4 rounded-md hover:bg-[#009688] focus:outline-none focus:bg-[#009688] justify-center" : `w-full bg-[#007373] text-white py-2 px-4 rounded-md hover:bg-[#009688] focus:outline-none focus:bg-[#009688] justify-center`}`}
+            className={`${loading ? "w-full bg-[white] border-2 border-[#007373] text-white py-2 px-4 rounded-md justify-center" : `w-full bg-[#007373] text-white py-2 px-4 rounded-md hover:bg-[#009688] focus:outline-none focus:bg-[#009688] justify-center`}`}
           >
-            {loading ? <div className='flex justify-center'><img alt='excel' src={excel} className='h-5 w-5 animate-pulse'/><p className='ml-2 my-auto text-[#007373]'>Estimating</p></div>: <p>Submit</p>}
+            {loading ? <div className='flex justify-center'><img alt='excel' src={excel} className='h-5 w-5 animate-pulse' /><p className='ml-2 my-auto text-[#007373]'>Estimating</p></div> : <p>Submit</p>}
           </button>
           {error &&
-          <div className='flex justify-center mt-2 bg-[#FFCDD2] border-2 border-[#EF5350] rounded-sm'>
-             <label className="block text-sm font-medium text-gray-700 justify-center py-2">
-              Error, Please try again
-            </label>
-          </div>
+            <div className='flex justify-center mt-2 bg-[#FFCDD2] border-2 border-[#EF5350] rounded-sm'>
+              <label className="block text-sm font-medium text-gray-700 justify-center py-2">
+                Error, Please try again
+              </label>
+            </div>
           }
         </div>
       </form>
