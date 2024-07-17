26c71ac59c7365960c4961a622b06d53135da924
piyush.patel <piyush.patel@rws.com>
2024-07-17 17:47:35 +0530

    rws logo light

--- estimation-helper/src/Components/SmallChunks/RWSLogo.tsx
+++ estimation-helper/src/Components/SmallChunks/RWSLogo.tsx
@@ -2,7 +2,7 @@ const RWS = () => {
     return (
         <div className="flex">
         <img src={require('../../assets/rws.png')} className="h-6"/>
-        <p className="font-100 ml-2 my-auto text-white text-[20px] tracking-wide">RWS</p>
+        <p className="font-light ml-1 my-auto text-white text-[23px] tracking-tighter">RWS</p>
         </div>
     )
 }
